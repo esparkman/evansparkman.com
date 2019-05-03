d4a0bf680adff1321b8c133ddc065c6b0abb1971
Evan Sparkman <me@evansparkman.com>
2019-05-03 12:47:26 -0500

    Revert Layout

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import { Link, graphql, useStaticQuery } from "gatsby"
 
 import blogStyles from "./blog.module.scss"
