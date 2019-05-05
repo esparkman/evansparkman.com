316242965e8bae03fb21a0763152af223c82208d
Evan Sparkman <me@evansparkman.com>
2019-05-05 13:27:32 -0400

    Move uploads folder

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,35 +1,26 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 import Carousel from "nuka-carousel"
 
-export default class extends React.Component {
-  render() {
-    return (
-      <Carousel>
-        <img
-          alt="Heyo 1"
-          src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1"
-        />
-        <img
-          alt="Heyo 1"
-          src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide2"
-        />
-        <img
-          alt="Heyo 1"
-          src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3"
-        />
-        <img
-          alt="Heyo"
-          src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4"
-        />
-        <img
-          alt="Heyo"
-          src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide5"
-        />
-        <img
-          alt="Heyo 1"
-          src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide6"
-        />
-      </Carousel>
-    )
-  }
+const Gallery = () => {
+  const gallery = useStaticQuery(graphql`
+    query {
+      markdownRemark(fields: { slug: { eq: "2018-2019" } }) {
+        frontmatter {
+          title
+          gallery_images
+        }
+      }
+    }
+  `)
+
+  return (
+    <Carousel>
+      {gallery.markdownRemark.frontmatter.gallery_images.map((image, index) => {
+        return <img alt="Heyo 1" src={image} key={index} />
+      })}
+    </Carousel>
+  )
 }
+
+export default Gallery
